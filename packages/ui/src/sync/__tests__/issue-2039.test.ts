7e7ca95f932f3d0e85b642caaf05ce194910107d
Yujiang Wang <yujiang.wang.rock@gmail.com>
2026-08-14 17:07:08 +0800

    Local: bound revert history loading

--- packages/ui/src/sync/__tests__/issue-2039.test.ts
+++ packages/ui/src/sync/__tests__/issue-2039.test.ts
@@ -345,7 +345,7 @@ mock.module("../session-actions", () => ({
   shareSession: mock(async () => undefined),
   unshareSession: mock(async () => undefined),
   optimisticSend: mock(async () => undefined),
-  refetchSessionMessages: mock(async () => undefined),
+  ensureSessionMessageRequirement: mock(async () => false),
   revertToMessage: mock(async () => undefined),
   unrevertSession: mock(async () => undefined),
   forkFromMessage: mock(async () => undefined),
@@ -353,6 +353,7 @@ mock.module("../session-actions", () => ({
   getSessionLastAssistantModel: () => null,
   patchSessionMetadata: mock(async () => undefined),
   abortCurrentOperation: mock(async () => undefined),
+  stopSessionExecution: mock(async () => undefined),
 }))
 
 mock.module("@/lib/git/branchNameGenerator", () => ({
