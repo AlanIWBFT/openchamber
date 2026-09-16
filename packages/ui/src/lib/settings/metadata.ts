2a4051987c4d35a3c0f7443c86f5081cdd6e27e0
Bohdan Triapitsyn <artmore@protonmail.com>
2026-09-16 12:46:06 +0300

    style: use puzzle icon for extensions settings page
    Extensions settings nav now uses a puzzle icon

--- packages/ui/src/lib/settings/metadata.ts
+++ packages/ui/src/lib/settings/metadata.ts
@@ -300,7 +300,7 @@ export function getSettingsNavIcon(slug: SettingsPageSlug): IconName | null {
     case 'integrations':
       return 'plug';
     case 'extensions':
-      return 'window';
+      return 'puzzle';
 
     case 'usage':
       return 'bar-chart-2';
